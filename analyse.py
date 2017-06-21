08bcb1f920fb0aed8a459157390aea4ed8debd68
sirkibsirkib <christopher.esterhuyse@gmail.com>
2017-06-21 14:41:29 +0200

    fix

--- analyse.py
+++ analyse.py
@@ -3,6 +3,7 @@ import sys
 
 if len(sys.argv) < 3:
 	print('USAGE: [excluded ID list] [file1path] [file2path]...')
+	exit()
 excepted_list = [int(line.rstrip('\n')) for line in open('./data/viral/removed_IDs.txt')]
 venn_walk(excepted_list,
 		  sys.argv[2:])
